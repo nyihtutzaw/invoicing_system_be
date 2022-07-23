408f77af54c398e8933d63126709a388c9c92665
nyihtutzaw <nyihtutzaw.2015@gmail.com>
2022-07-23 21:55:51 +0630

    modify product controller for keyword search (#8)

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,7 +1,14 @@
 import ProductModel from './../model/product'
+import { Op } from 'sequelize'
 class ProductController {
   async index(req, res) {
-    const products = await ProductModel.findAll()
+    let query = {}
+    if (req.query.search) {
+      query.name = {
+        [Op.like]: `%${req.query.search}%`,
+      }
+    }
+    const products = await ProductModel.findAll({ where: query })
     res.status(200).json({ products: products })
   }
 }
