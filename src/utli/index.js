67c400362cdbefd6127611811fcd7e3b48635b47
nyihtutzaw <nyihtutzaw.2015@gmail.com>
2022-07-23 21:45:49 +0630

    add pagination to invoice with reusable code

---
+++ src/utli/index.js
@@ -0,0 +1,21 @@
+export function getPaginationAttribute(req, totalCount) {
+  let limit = 10
+  let offset = 0
+  let page = 1
+  if (req.query.page) {
+    page = Number(req.query.page)
+    if (page > 1) {
+      offset = (page - 1) * limit
+    }
+  }
+
+  let totalPage = Math.round(totalCount / limit)
+  if (totalPage < 1) totalPage = 1
+
+  return {
+    limit,
+    offset,
+    page,
+    totalPage,
+  }
+}
